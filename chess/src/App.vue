0bc428c0b42f72c977ca7159ee62aaef44a5e517
Lucas Bateson <lucas.bateson@gmail.com>
2023-02-09 21:32:04 +0100

    Working board

--- chess/src/App.vue
+++ chess/src/App.vue
@@ -1,3 +1,29 @@
+<!-- Chess game-->
 <template>
-
+    <div class="chess">
+        <table>
+            <tr v-for="(row, i) in board" :key="i" :class="{ 'evenRow': i % 2 == 0, 'oddRow': i % 2 != 0 }">
+                <td v-for="(cell, j) in row" :key="j" :class="{ 'evenTile': j % 2 == 0, 'oddTile': j % 2 != 0 }">
+                    <Piece :piece="cell" />
+                </td>
+            </tr>
+        </table>
+    </div>
 </template>
+
+<script>
+import { ref } from 'vue'
+import Piece from './components/Piece.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Piece
+  },
+  data() {
+    return {
+      board: [['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '']]
+    }
+  }
+}
+</script>
---
+++ chess/src/components/Piece.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="piece" @click="selectPiece">
+        <img :src="img" />
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+
+export default {
+    name: 'Piece',
+    data() {
+        return {
+            img: null
+        }
+    },
+    methods:{
+        selectPiece() {
+            console.log('piece selected')
+        },
+        getPieceImg() {
+            switch (this.piece) {
+                case 'wP':
+                    this.img = require('../assets/w/p.png')
+                    break
+                case 'wR':
+                    this.img = require('../assets/w/r.png')
+                    break
+                case 'wN':
+                    this.img = require('../assets/w/n.png')
+                    break
+                case 'wB':
+                    this.img = require('../assets/w/b.png')
+                    break
+                case 'wQ':
+                    this.img = require('../assets/w/q.png')
+                    break
+                case 'wK':
+                    this.img = require('../assets/w/k.png')
+                    break
+                case 'bP':
+                    this.img = require('../assets/b/p.png')
+                    break
+                case 'bR':
+                    this.img = require('../assets/b/r.png')
+                    break
+                case 'bN':
+                    this.img = require('../assets/b/n.png')
+                    break
+                case 'bB':
+                    this.img = require('../assets/b/b.png')
+                    break
+                case 'bQ':
+                    this.img = require('../assets/b/q.png')
+                    break
+                case 'bK':
+                    this.img = require('../assets/b/k.png')
+                    break
+                default:
+                    this.img = null
+                    break
+            }
+        }
+    },
+    mounted() {
+        this.getPieceImg()
+    }
+}
+</script>
