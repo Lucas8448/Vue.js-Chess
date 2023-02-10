4431726cde5b5cc30f22c7f6aba7c318a13b7048
Lucas Bateson <lucas.bateson@gmail.com>
2023-02-10 09:41:31 +0100

    Working movement

--- chess/src/App.vue
+++ chess/src/App.vue
@@ -3,8 +3,8 @@
     <div class="chess">
         <table>
             <tr v-for="(row, i) in board" :key="i" :class="{ 'evenRow': i % 2 == 0, 'oddRow': i % 2 != 0 }">
-                <td v-for="(cell, j) in row" :key="j" :class="{ 'evenTile': j % 2 == 0, 'oddTile': j % 2 != 0 }">
-                    <Piece :piece="cell" v-if="cell != ''"/>
+                <td v-for="(cell, j) in row" :key="j" :class="{ 'evenTile': j % 2 == 0, 'oddTile': j % 2 != 0 }" @click="selectPiece(i, j)">
+                    <Piece :piece="cell" v-if="cell != '' && cell !='*'"/>
                 </td>
             </tr>
         </table>
@@ -22,7 +22,31 @@ export default {
   },
   data() {
     return {
+      validPieces: ['wP', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bP', 'bR', 'bN', 'bB', 'bQ', 'bK'],
+      activeMove: null,
+      selectedPiece: null,
+      selectedPieceType: null,
       board: [['wR', 'wN', 'wB', 'wK', 'wQ', 'wB', 'wN', 'wR'], ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'], ['bR', 'bN', 'bB', 'bK', 'bQ', 'bB', 'bN', 'bR']]
+    }
+  },
+  methods: {
+    selectPiece(x, y) {
+      console.log(x, y)
+      this.selectedPieceType = this.board[x][y]
+      // move piece if there is one already selected, and if not select this piece
+      if (this.selectedPiece) {
+        this.activeMove = [x, y]
+        this.movePiece()
+      } else {
+        this.selectedPiece = [x, y]
+      }
+    },
+    movePiece() {
+      console.log("moving piece")
+      this.board[this.activeMove[0]][this.activeMove[1]] = this.board[this.selectedPiece[0]][this.selectedPiece[1]]
+      this.board[this.selectedPiece[0]][this.selectedPiece[1]] = ''
+      this.selectedPiece = null
+      this.activeMove = null
     }
   }
 }
