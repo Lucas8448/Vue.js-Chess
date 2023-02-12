3a1957abb501497e5dd0531c14031ceead377ae8
Lucas Bateson <lucas.bateson@gmail.com>
2023-02-12 20:21:06 +0100

    Working piece capture, and refactor of css

--- chess/src/App.vue
+++ chess/src/App.vue
@@ -4,8 +4,8 @@
     <h2 v-else>Black Move</h2>
     <div class="chess">
         <table>
-            <tr v-for="(row, i) in board" :key="i" :class="{ 'evenRow': i % 2 == 0, 'oddRow': i % 2 != 0 }">
-                <td v-for="(cell, j) in row" :key="j" :class="{ 'evenTile': j % 2 == 0, 'oddTile': j % 2 != 0 }" @mousedown="startPieceMove(i, j)" @mouseup="leavePiecemove(i, j)">
+            <tr v-for="(row, i) in board" :key="row" :class="{ 'evenRow': i % 2 == 0, 'oddRow': i % 2 != 0 }">
+                <td v-for="(cell, j) in row" :key="cell" :class="{ 'evenTile': j % 2 == 0, 'oddTile': j % 2 != 0 , 'move': cell == '*'}" @click="squareSelected(i, j)">
                     <Piece :piece="cell" v-if="cell != '' && cell !='*'"/>
                 </td>
             </tr>
@@ -28,46 +28,74 @@ export default {
       // turn 0 is white, 1 is black
       movePiece: null,
       startMove: null,
+      // create an array of all the valid pieces
       validPieces: ['wP', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bP', 'bR', 'bN', 'bB', 'bQ', 'bK'],
       board: [['wR', 'wN', 'wB', 'wK', 'wQ', 'wB', 'wN', 'wR'], ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'], ['bR', 'bN', 'bB', 'bK', 'bQ', 'bB', 'bN', 'bR']]
     }
   },
   methods: {
-    // move piece from startPieceMove to endPieceMove
-    getPieceColour(x, y) {
-      if (this.board[x][y] == '') {
-        return ''
-      } else {
-        return this.board[x][y].charAt(0)
-      }
+    // basic checking functions
+    isPiece(piece) {
+      return this.validPieces.includes(piece)
     },
-    getPieceType(x, y) {
-      if (this.board[x][y] == '') {
-        return ''
-      } else {
-        return this.board[x][y].charAt(1)
-      }
+    isWhite(piece) {
+      return piece[0] == 'w'
     },
-    startPieceMove(i, j) {
-      if ((this.turn == 0 && this.getPieceColour(i, j) == 'w') || (this.turn == 1 && this.getPieceColour(i, j) == 'b')) {
-        console.log(this.getPieceColour(i,j))
-        this.movePiece = this.board[i][j]
-        this.startMove = [i, j]
-      }
+    isBlack(piece) {
+      return piece[0] == 'b'
     },
-    leavePiecemove(i, j) {
-      this.pieceMove(this.startMove[0], this.startMove[1], i, j)
+    isPawn(piece) {
+      return piece[1] == 'P'
+    },
+    isRook(piece) {
+      return piece[1] == 'R'
+    },
+    isKnight(piece) {
+      return piece[1] == 'N'
+    },
+    isBishop(piece) {
+      return piece[1] == 'B'
+    },
+    isKing(piece) {
+      return piece[1] == 'K'
+    },
+    isQueen(piece) {
+      return piece[1] == 'Q'
+    },
+    pieceType(row, col) {
+      return this.board[row][col][1]
+    },
+    // check if the piece is in the same team as the turn
+    isSameTeam(piece) {
       if (this.turn == 0) {
-        this.turn = 1
+        return this.isWhite(piece)
       } else {
-        this.turn = 0
+        return this.isBlack(piece)
       }
     },
-    pieceMove(sourceX, sourceY, targetX, targetY) {
-      var piece = this.board[sourceX][sourceY]
-      this.board[sourceX][sourceY] = ''
-      this.board[targetX][targetY] = piece
-    }
+    // select piece
+    squareSelected(row, col) {
+      if (this.movePiece == null) {
+        if (this.isPiece(this.board[row][col]) && this.isSameTeam(this.board[row][col])) {
+          this.movePiece = this.board[row][col]
+          this.startMove = [row, col]
+        }
+      } else {
+        if (this.isPiece(this.board[row][col]) && this.isSameTeam(this.board[row][col])) {
+          this.movePiece = this.board[row][col]
+          this.startMove = [row, col]
+        } else {
+          this.board[row][col] = ''
+          this.board[row][col] = this.movePiece
+          //clear target square even if there is an enemy piece there
+          this.board[this.startMove[0]][this.startMove[1]] = ''
+          this.movePiece = null
+          this.startMove = null
+          this.turn = (this.turn + 1) % 2
+        }
+      }
+      this.$forceUpdate();
+    },
   }
 }
 
